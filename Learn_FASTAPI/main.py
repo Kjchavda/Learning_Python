940a09866034e842e8172602cd7277832d83a184
Kjchavda <kulanjaychavda@gmail.com>
2025-03-29 16:04:17 +0530

    connecting to a database

--- Learn_FASTAPI/main.py
+++ Learn_FASTAPI/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException, Response, status
+import psycopg2.extras
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
@@ -9,6 +12,16 @@ class Post(BaseModel):
     content: str
     
     published: bool = True
+
+try:
+    conn = psycopg2.connect(host="localhost", database="Learn_FastAPI", user="postgres", password="kj0910", cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print("success")
+
+except Exception as error:
+    print("failed")
+    print("Error:" , error)
+
 
 my_posts = [{"title": "title1", "content": "content1", "id":1}, {"title": "fav food", "content": "pizza", "id":2}]
 
