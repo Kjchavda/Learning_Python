b7a6d17713bef43d6ebc2008651383c59a5dc16f
Kjchavda <kulanjaychavda@gmail.com>
2025-03-30 00:36:13 +0530

    INtegrated SQL with python

--- Learn_FASTAPI/main.py
+++ Learn_FASTAPI/main.py
@@ -31,14 +31,17 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    
+    return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)    #status code parameter to select default status code
 def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0,1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict }
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 def find_post(id):
     for p in my_posts:
@@ -52,35 +55,60 @@ def find_post_index(id):
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response): #No need to pass in response now
-    post = find_post(id)
+    """
+        post = find_post(id)
     if not post:
-        #Below code is a sloppy way to implement HTTP error
-        # response.status_code = status.HTTP_404_NOT_FOUND    #can also be hardcode to 404\
-        # return {"msg": f"post with id {id} was not found"}
-        # a better way to do this will be the following, raising HTTPException
+            #Below code is a sloppy way to implement HTTP error
+            # response.status_code = status.HTTP_404_NOT_FOUND    #can also be hardcode to 404
+            # return {"msg": f"post with id {id} was not found"}
+            # a better way to do this will be the following, raising HTTPException
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    """
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchone()
+
+    if not post:
+            #Below code is a sloppy way to implement HTTP error
+            # response.status_code = status.HTTP_404_NOT_FOUND    #can also be hardcode to 404
+            # return {"msg": f"post with id {id} was not found"}
+            # a better way to do this will be the following, raising HTTPException
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    
     return {"post_detail": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
+    """
     post_index = find_post_index(id)
     post = find_post(id)
-    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
     else:
         my_posts.pop(post_index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+    """
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    deleted_post = cursor.fetchone()
+    if not deleted_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    
+    conn.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    post_index = find_post_index(id)
-    
-    
-    if post_index == None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id),)) 
+    #post_index = find_post_index(id)
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
 
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[post_index] = post_dict
-    return{"data": post_dict}
+    #post_dict = post.dict()
+    #post_dict["id"] = id
+    #my_posts[post_index] = post_dict
+    return{"data": updated_post}
